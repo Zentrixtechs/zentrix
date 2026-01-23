4a7af0cef12e5a048c468d4a8298a8cf89af6c69
Balajimarpally <balajimarpally@gmail.com>
2026-01-23 23:35:49 +0530

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,18 @@ import type { ReactNode } from "react";
 import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Zentrix',
+  description: 'Marketing Services',
+  // 👇 ADD THIS SECTION
+  icons: {
+    icon: '/images/so.png', // Path to your logo
+    shortcut: '/images/so.png',
+    apple: '/images/so.png',
+  },
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
